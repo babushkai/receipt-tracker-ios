73067d7b7554a1d6cc621a8f61d034c2815ac91e
kwdaisuke <daisuke0582@gmail.com>
2025-10-19 10:00:32 +0200

    feat: add unit test

---
+++ ReceiptTrackerTests/ReceiptTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReceiptTrackerTests.swift
+//  ReceiptTrackerTests
+//
+//  Created by dsuke on 19/10/2025.
+//
+
+import Testing
+
+struct ReceiptTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
